56e2e7caf1aad12fd21c3d79f5d766b63ee67bda
vignesh <vigneshachari06@gmail.com>
2021-07-19 17:17:12 +0530

    Updated

---
+++ js/Script.js
@@ -0,0 +1,145 @@
+$('.owl-carousel').owlCarousel({
+
+    margin: 20,
+    nav: true,
+    navText: ["<div class='nav-button owl-prev'>‹</div>", "<div class='nav-button owl-next'>›</div>"],
+    responsive: {
+      0: {
+        items: 1
+      },
+      500: {
+        items: 1
+      },
+      1000: {
+        items: 3
+      }
+    }
+});
+
+$(document).ready(function(){
+
+  // In case to load page already filtered by category
+
+  setTimeout(function(){
+    $('#catone').click();
+  },100)
+  $('.category_items').click(function(){
+    $(this).addClass('category_items_clicked').siblings().removeClass('category_items_clicked');
+
+
+    var category = $(this).attr('id');
+      $('.any_item').addClass('magic');
+      
+      setTimeout(function(){ $('.any_item').css('display','none'); },600)
+        setTimeout(function(){
+          $('.'+category).removeClass('magic');
+
+        },800);
+        setTimeout(function(){ $('.'+category).css('display','block'); },600)
+
+
+  });
+
+});
+
+(function($) {
+
+	"use strict";
+
+
+  // Form
+	var contactForm = function() {
+		if ($('#contactForm').length > 0 ) {
+			$( "#contactForm" ).validate( {
+				rules: {
+					name: "required",
+					subject: "required",
+					email: {
+						required: true,
+						email: true
+					},
+					message: {
+						required: true,
+						minlength: 5
+					}
+				},
+				messages: {
+					name: "Please enter your name",
+					subject: "Please enter your subject",
+					email: "Please enter a valid email address",
+					message: "Please enter a message"
+				},
+				/* submit via ajax */
+				
+				submitHandler: function(form) {		
+					var $submit = $('.submitting'),
+						waitText = 'Submitting...';
+
+					$.ajax({   	
+				      type: "POST",
+				      url: "https://bigappcompany.co.in/demos/cms/contactcompany",
+				      data: $(form).serialize(),
+
+				      beforeSend: function() { 
+				      	$submit.css('display', 'block').text(waitText);
+				      },
+				      success: function(msg) {
+		               if (msg == 'OK') {
+		               	$('#form-message-warning').hide();
+				            setTimeout(function(){
+		               		$('#contactForm').fadeIn();
+		               	}, 1000);
+				            setTimeout(function(){
+				               $('#form-message-success').fadeIn();   
+		               	}, 1400);
+
+		               	setTimeout(function(){
+				               $('#form-message-success').fadeOut();   
+		               	}, 8000);
+
+		               	setTimeout(function(){
+				               $submit.css('display', 'none').text(waitText);  
+		               	}, 1400);
+
+		               	setTimeout(function(){
+		               		$( '#contactForm' ).each(function(){
+											    this.reset();
+											});
+		               	}, 1400);
+			               
+			            } else {
+			               $('#form-message-warning').html(msg);
+				            $('#form-message-warning').fadeIn();
+				            $submit.css('display', 'none');
+			            }
+				      },
+				      error: function() {
+				      	$('#form-message-warning').html("Something went wrong. Please try again.");
+				         $('#form-message-warning').fadeIn();
+				         $submit.css('display', 'none');
+				      }
+			      });    		
+		  		} // end submitHandler
+
+			});
+		}
+	};
+	contactForm();
+
+})(jQuery);
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+	this.classList.toggle("active-1");
+	var panel = this.nextElementSibling;
+	if (panel.style.maxHeight){
+	  panel.style.maxHeight = null;
+	} else {
+	  panel.style.maxHeight = panel.scrollHeight + "px";
+	} 
+  });
+}
---
+++ js/owl-carosuel.js
@@ -0,0 +1,58 @@
+<div class="products">
+<div class="container carousel-wrapper">
+  <div class="row">
+    <div class="col-md-12">
+      <div class="section-heading">
+        <h2 class=" product-title">Our Products</h2>
+        <div class="productHR"><span></span></div>
+      </div>
+    </div>
+    <div class="owl-carousel owl-theme">
+        <div class="Device1">
+            <div class="products-item">
+              <img src="img/Intellistain.png" alt="">
+              <div class="down-content">
+                <h4>Intellistain</h4>
+                <p>Aindra IS is an automated stainer for staining biological samples that are mounted on glass slides. Aindra IS uses state of the art mechatronics and software to 
+                    achieve unparalled staining.This sleek, compact and portable device.</p>
+                <a href="" class="filled-button">Read More</a>
+              </div>
+            </div>
+        </div>
+        <div class="Device2">
+            <div class="products-item">
+              <img src="img/VisionX.png" alt="">
+              <div class="down-content">
+                <h4>VisionX</h4>
+                <p>Tele-pathology: Aindra VisionX gives extremely Crisp images of a whole slide . These image enable extremely fast turn around times for the report by enabling 
+                   images captured at the point of care to be transferred to a Clinician, thus enabling telepatholgy.</p>
+                <a href="" class="filled-button">Read More</a>
+              </div>
+            </div>
+        </div>
+        <div class="Device3">
+            <div class="products-item">
+              <img src="img/Astra.png" alt="">
+              <div class="down-content">
+                <h4>Astra</h4>
+                <p>Aindra Astra is an AI platform developed to facilitate computational pathology for faster and more accurate diagnosis.<br>
+                   Astra is built from data that is assessed by experienced senior pathologists.</p>
+                <a href="" class="filled-button">Read More</a>
+              </div>
+            </div>
+        </div>
+        <div class="Device3">
+            <div class="products-item">
+              <img src="img/Astra.png" alt="">
+              <div class="down-content">
+                <h4>Astra</h4>
+                <p>Aindra Astra is an AI platform developed to facilitate computational pathology for faster and more accurate diagnosis.<br>
+                   Astra is built from data that is assessed by experienced senior pathologists.</p>
+                <a href="" class="filled-button">Read More</a>
+              </div>
+            </div>
+        </div>
+    </div>
+  </div>
+</div>
+</div>
